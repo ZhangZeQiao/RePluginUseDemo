80b0eb61a6498dc64afab80114d47cfc7eb6c262
SKY-20160523ZQP\admin <571129524@qq.com>
2017-08-11 17:10:45 +0800

    内置插件的升级、卸载（删除）

--- app/src/main/java/com/xq/repluginusedemo/MainActivity.java
+++ app/src/main/java/com/xq/repluginusedemo/MainActivity.java
@@ -1,12 +1,18 @@
 package com.xq.repluginusedemo;
 
+import android.app.Activity;
+import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
+import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 
 import com.qihoo360.replugin.RePlugin;
 
 public class MainActivity extends AppCompatActivity {
+
+    private UpdateService mUpdateService;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,5 +25,32 @@ public class MainActivity extends AppCompatActivity {
         RePlugin.startActivity(MainActivity.this,
                 RePlugin.createIntent("image", "com.xq.imageplugindemo.MainActivity"));
 
+    }
+
+    public void onUpdateImagePlugin(View view) {
+        verifyStoragePermissions(this);
+        Intent intent = new Intent(this, UpdateService.class);
+        startService(intent);
+    }
+
+    private static final int REQUEST_EXTERNAL_STORAGE = 1;
+    private static String[] PERMISSIONS_STORAGE = {
+            "android.permission.READ_EXTERNAL_STORAGE",
+            "android.permission.WRITE_EXTERNAL_STORAGE"};
+
+    /**
+     * 动态申请读写权限
+     **/
+    public static void verifyStoragePermissions(Activity activity) {
+        try {
+            //检测是否有写的权限
+            int permission = ActivityCompat.checkSelfPermission(activity, "android.permission.WRITE_EXTERNAL_STORAGE");
+            if (permission != PackageManager.PERMISSION_GRANTED) {
+                // 没有写的权限，去申请写的权限，会弹出对话框
+                ActivityCompat.requestPermissions(activity, PERMISSIONS_STORAGE, REQUEST_EXTERNAL_STORAGE);
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 }
---
+++ app/src/main/java/com/xq/repluginusedemo/UpdateService.java
@@ -0,0 +1,95 @@
+package com.xq.repluginusedemo;
+
+import android.app.IntentService;
+import android.content.Intent;
+import android.os.Environment;
+import android.support.annotation.Nullable;
+import android.util.Log;
+
+import com.qihoo360.replugin.RePlugin;
+
+import java.io.BufferedInputStream;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.OutputStream;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.net.URLConnection;
+
+/**
+ * @author 小侨
+ * @time 2017/8/11  10:24
+ * @desc 更新插件
+ */
+
+public class UpdateService extends IntentService {
+
+    // 插件下载地址
+    String urlPath = "https://raw.githubusercontent.com/ZhangZeQiao/ImagePluginDemo/7c5866db83b57c455302fac12ea72af30d9a5364/app/src/main/assets/image.apk";
+    // 插件下载后的存放路径
+    String downloadDir = Environment.getExternalStorageDirectory().getAbsolutePath();
+
+    public UpdateService() {
+        // 实现父类的构造方法，用于命名工作线程，只用于调试。
+        super("UpdateService");
+    }
+
+    @Override
+    protected void onHandleIntent(@Nullable Intent intent) {
+        // Intent是从Activity发过来的，携带识别参数，根据参数不同执行不同的任务
+
+        File file = null;
+        try {
+            // 统一资源
+            URL url = new URL(urlPath);
+            // 连接类的父类，抽象类
+            URLConnection urlConnection = url.openConnection();
+            // http的连接类
+            HttpURLConnection httpURLConnection = (HttpURLConnection) urlConnection;
+            // 设定请求的方法，默认是GET
+            httpURLConnection.setRequestMethod("GET");
+            // 设置字符编码
+            httpURLConnection.setRequestProperty("Charset", "UTF-8");
+            // 打开到此 URL 引用的资源的通信链接（如果尚未建立这样的连接）。
+            httpURLConnection.connect();
+
+            // 文件大小
+            int fileLength = httpURLConnection.getContentLength();
+            // 文件名
+            String filePathUrl = httpURLConnection.getURL().getFile();
+            String fileFullName = filePathUrl.substring(filePathUrl.lastIndexOf(File.separatorChar) + 1);
+
+            URLConnection con = url.openConnection();
+            BufferedInputStream bin = new BufferedInputStream(httpURLConnection.getInputStream());
+
+            String path = downloadDir + File.separatorChar + fileFullName;
+            file = new File(path);
+            if (!file.getParentFile().exists()) {
+                file.getParentFile().mkdirs();
+            }
+            OutputStream out = new FileOutputStream(file);
+            int size = 0;
+            int len = 0;
+            byte[] buf = new byte[1024];
+            while ((size = bin.read(buf)) != -1) {
+                len += size;
+                out.write(buf, 0, size);
+                // 下载百分比
+                Log.v("xq", "下载了-------> " + len * 100 / fileLength);
+            }
+            bin.close();
+            out.close();
+            // 升级安装插件新版本
+            RePlugin.install(path);
+            Log.v("xq", "更新完成");
+        } catch (MalformedURLException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+
+        }
+    }
+}
